22314382aa9c908ab7ef65e2ba0facbae258cf3d
Aravindh-S <soniaravindh@gmail.com>
2018-02-08 11:19:59 +0530

    COMPLETED WITH PEP8 AND PLOT

--- machine_learning/assignments/src/logistic_regression.py
+++ machine_learning/assignments/src/logistic_regression.py
@@ -1,47 +1,52 @@
+'''
+Logistic Regression
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-li = []
-pi = []
+LIL = []
+PIP = []
 with open('x.dat', 'r') as xd:
     with open('y.dat', 'r') as yd:
-        data = xd.readlines()
-        check = yd.readlines()
-        for i in data:
-            li.append(i.strip().split())
-        for j in check:
-            pi.append(float(j.strip()))
+        DATA = xd.readlines()
+        CHECK = yd.readlines()
+        for i in DATA:
+            LIL.append(i.strip().split())
+        for j in CHECK:
+            PIP.append(float(j.strip()))
 
-        X, xr, yr, xb, yb = [], [], [], [], []
-        for i, j in enumerate(li):
+        X, XR, YR, XB, YB = [], [], [], [], []
+        for i, j in enumerate(LIL):
             X.append([float(j[0]), float(j[1])])
 
-            if(pi[i] == 0):
-                xr.append(float(j[0]))
-                yr.append(float(j[1]))
+            if PIP[i] == 0:
+                XR.append(float(j[0]))
+                YR.append(float(j[1]))
 
             else:
-                xb.append(float(j[0]))
-                yb.append(float(j[1]))
+                XB.append(float(j[0]))
+                YB.append(float(j[1]))
 
-        plt.scatter(xr, yr, color="red")
-        plt.scatter(xb, yb)
-        plt.show()
+        plt.scatter(XR, YR, color="red")
+        plt.scatter(XB, YB)
 
         U = (np.array(X))
 
-        check = np.array([0.02, 0.08])
-        ch = np.transpose([check])
-        V = np.array(pi)
-        print(U)
-        print('\n')
-        print(V)
+        V = np.array(PIP)
 
-        print(ch)
+        LOGREG = LogisticRegression()
+        LOGREG.fit(U, V)
 
-        logreg = LogisticRegression()
-        logreg.fit(U, V)
+        # t = logreg.predict(np.array([[0.34792627, 0.8625731]]))
 
-        t = logreg.predict(np.array([[0.34792627, 0.8625731]]))
-        print("Here is the output")
-        print(t)
+        M = LOGREG.coef_[0][0]
+        C = LOGREG.coef_[0][1]
+
+        X2 = max(max(XB), max(XR))
+        X1 = min(min(XR), min(XB))
+        Y1 = X1 * M + C
+        Y2 = X2 * M + C
+        XP = [X1, X2]
+        YP = [Y1, Y2]
+        plt.plot(XP, YP)
+        plt.show()
